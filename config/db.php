5a9f922a8d096065a7a105a7df047dda945cffa2
unknown <sulthanrafif69@gmail.com>
2019-11-04 16:02:39 +0700

    tambah file github

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=e-surat',
+    'username' => 'root',
+    'password' => 'Affan123*',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
